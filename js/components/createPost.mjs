fae752b2ce722e17b203cda38d68879c29ab8a56
AdrianMikk <adrmik99436@stud.noroff.no>
2023-10-15 16:34:29 +0200

    create post bugfix undo

--- js/components/createPost.mjs
+++ js/components/createPost.mjs
@@ -18,7 +18,7 @@ export async function createNewPost(options) {
     try {
         const response = await apiFetch(fullPostURL, options);
 
-        if (response) {
+        if (response && response.id) {
             fetchAndDisplayPosts();
             createPostForm.reset();
         } else {
